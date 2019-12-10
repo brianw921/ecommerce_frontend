3992267a807c97621382028cc1f37d3d0d10db18
Brian Wong <bwong92@gmail.com>
2019-12-10 17:57:44 -0500

    cart CSS

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -22,24 +22,36 @@ class Cart extends Component {
         const cartPrice = () => {
             let totalPrice = 0
             cart.forEach((cartItem) => {
-                
                 totalPrice += cartItem.item.original_price
             })
             return totalPrice
         }
+
+        const shoppingCartItems = () => {
+            if (cart.length === 0 ){
+                return "Your Shopping Cart is Empty"
+            } else if (cart.length === 1) {
+                return `There is currently ${cart.length} item in your Cart`
+            } else {
+                return `There are currently ${cart.length} items in your Cart`
+            }
+        }
         return (
-            <div>
-                <h1>There are currently {cart.length} items in your Cart</h1>
-                <ol>
-                    {cart.map((cartItem) => {
-                        
-                        return <li>{cartItem.item.product_full_name} USD {cartItem.item.original_price}
-                         <button onClick={() => this.removeFromCart(cartItem)}>Remove from Cart</button></li>
-                    })}
-                    <div>
+            <div className="cart-show-page">
+                <h1>{shoppingCartItems()} </h1>
+                <div>
+                    <ol>
+                        {cart.map((cartItem) => {
+                            console.log(cartItem)
+                            return <li>
+                            <img src={cartItem.item.image} alt="" className="cart-img"/>- {cartItem.item.product_full_name} USD {cartItem.item.original_price}
+                            <button onClick={() => this.removeFromCart(cartItem)}>Remove from Cart</button></li>
+                        })}
+                    </ol>
+                </div>
+                <div className="cart-price">
                         <h1>Total Price: <span>{cartPrice()}</span></h1>
-                    </div>
-                </ol>
+                </div>
                 <button onClick={this.submitOrder}>Checkout</button>
             </div>
         )
--- src/Pages/OrderPopup.js
+++ src/Pages/OrderPopup.js
@@ -29,7 +29,7 @@ class OrderPopup extends Component {
                     {/* <div style={{'height': '200px','backgroundImage': `url(${image})`, 'backgroundSize': 'contain', 'backgroundRepeat': 'no-repeat'}}></div> */}
                     <img src={image} style={{'width': '300px', 'height': '300px'}}  alt="product-img"/>
                     <div style={{'width': '70%'}}>
-                        <div className="close-btn" onClick={this.props.history.goBack}>x</div>
+                        
                         <h2> YOU HAVE SUCCESSFULLY ADDED TO CART</h2>
                         <h2>{product_full_name}</h2>
                         
@@ -38,12 +38,11 @@ class OrderPopup extends Component {
                         <p>{description_headline}</p>
                         <h4> USD <span>{original_price}</span></h4> 
                         <ul>{description_bullets}</ul>
-
-                    </div>
-                    <div>
                         <button onClick={this.viewCart}>View Cart</button>
                         <button onClick={this.redirect}>Continue Shopping</button>
+
                     </div>
+                    <div className="close-btn" onClick={this.props.history.goBack}>x</div>
                 </div>
             </div>
             
