06406029a475ab1b12d58ea0e99897d53049c248
Brian Wong <bwong92@gmail.com>
2020-01-10 20:54:27 -0500

    react redux

--- src/Pages/index.js
+++
@@ -1,8 +0,0 @@
-// import Signup from './Signup'
-// import Login from './Login'
-// import ProductShowPage from './ProductShowPage'
-// export default {
-//     Login,
-//     Signup,
-//     ProductShowPage
-// }
